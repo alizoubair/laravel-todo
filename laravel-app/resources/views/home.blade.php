e0bb8b769b12e86bc5152f2b9e01f729c5e1b4c4
Ali Zoubair <88006585+alizoubair@users.noreply.github.com>
2023-12-16 07:44:49 +0100

    Initial commit

---
+++ laravel-app/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to Laravel ToDo App</h1>
+        <hr/>
+
+        @include('partials.flash_notification')
+
+        <p>For any query please contact</p>
+    </div>
+@endsection
---
+++ laravel-app/resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<hr/>
+
+<div class="container">
+    &copy; {{ date('Y') }}, <a href="http://google.com">Ali Zoubair</a>
+    <br/>
+</div>
